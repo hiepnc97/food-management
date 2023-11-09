9b55171732481bd5a2f118926992e561398ab81b
hiepnc <nguyenchanhiep197@gmail.com>
2023-11-09 16:38:54 +0700

    build: create app

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import * as path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/_mixins.scss";`,
+      },
+    },
+  },
+})
